eb23400eb73c4dde04824cd6b423826161f0aeb8
itschoolgomother <itschoolgomother@gmail.com>
2020-01-19 19:31:54 +0200

    19.1.2020

---
+++ ivanp/main.js
@@ -0,0 +1,10 @@
+
+
+// alert(String.fromCharCode(97));
+alert("Dima".charCodeAt(0))
+var str = "Admin"
+
+for(var i = 0;i< string.lenght-1;i++){
+	alert(string[i]. charCodeAt(0))
+}
+var string = str.split('')
