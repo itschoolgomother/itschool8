f931c7146110f8b4f05e220eb5c91334830af9c4
itschoolgomother <itschoolgomother@gmail.com>
2020-10-12 18:59:52 +0300

    12.10.2020

---
+++ Yura/js.js
@@ -0,0 +1,12 @@
+function meetUP(C,N){
+	let meets = (12 - C)/N;
+	return Math.floor(meets);
+
+}
+let num = 5;
+console.log("num");	
+
+function exchange(vidValuti,curs,summa){
+	let colichestvo  = curs * summa;
+return`Мы вам дадим ${colichestvo} ${vidValuti}`;
+}
