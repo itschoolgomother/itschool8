8378c18413dbb0a7f2674b2e47e5a88c08d32d65
itschoolgomother <itschoolgomother@gmail.com>
2020-10-31 16:34:17 +0200

    31.10.2020

---
+++  Bogdan/js.js
@@ -0,0 +1,42 @@
+/*function meetUp(N, C) {
+	let meets = (12-C)/N;
+ return  Math.floor(meets);
+}
+
+let num = 5;
+console.log("num")
+
+function getExchange(vidVolut,kyrs,summa){
+	let kolvo = kyrs*summa;
+ return `Mi Vam dadim  ${kolvo} ${vidVolut}`;
+}
+function litres (time){
+	return Math.floor(time*0.5);
+}
+function sravnenie(){
+let a = true;
+let b = false;
+if(a ==!b){
+	return  true;
+}else{
+	return false;
+}
+}
+let cashInWallet=30;
+let amount = 50 ;
+const prias = 40;
+let canBay = (cashInWallet>=prias);
+let  enoughWalet = (cashInWallet>=prias);
+let enoughCasol=amount>=prias;
+let canBuy= enoughWalet || enoughCasol;
+console.log(canBay);
+let isWaterHot = true;
+let makeTea = true;
+isWaterHot = false;
+let canMakeTea = isWaterHot && makeTea;
+console.log(canMakeTea)*/
+
+let str = "Hello Worold";
+for(let i=0;i<= 10;i++){
+console.log(str[i])
+}
