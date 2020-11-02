b3cd2137f0e35c209460cfa22b4eab3d7738ce08
itschoolgomother <itschoolgomother@gmail.com>
2020-11-02 09:57:58 +0200

    2.11.2020

--- Yura/js.js
+++ Yura/js.js
@@ -1,4 +1,4 @@
-function meetUP(C,N){
+/*function meetUP(C,N){
 	let meets = (12 - C)/N;
 	return Math.floor(meets);
 
@@ -9,4 +9,30 @@ console.log("num");
 function exchange(vidValuti,curs,summa){
 	let colichestvo  = curs * summa;
 return`Мы вам дадим ${colichestvo} ${vidValuti}`;
+}*/
+
+/*function litres(time){
+	return Math.floor(time * 0.5);
+    
 }
+
+let a = true;
+let b = false;
+ function srvn() {
+ 	if(a == b) {
+ 		return true;
+ 	}else{
+ 		return false;
+ 	}
+ }
+ let SkokDeneg = 30;
+ let SkokDeneg2 = 50;
+ let price = 40;
+ let DostatochnoVcoshelke = SkokDeneg >=price;
+ let DostatochnoNaKarte = SkokDeneg2 >= price;
+ let MojeshCupit = DostatochnoVcoshelke || DostatochnoNaKarte;
+ console.log(MojeshCupit); */ 
+ let waterhat = true;
+ let make tea = true
+     is waterhat = false;
+  
