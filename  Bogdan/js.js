4d3800bcb7aa54dca5e4a706e90c29b238140b60
itschoolgomother <itschoolgomother@gmail.com>
2020-11-14 11:59:23 +0200

    14.11.2020

---  Bogdan/js.js
+++  Bogdan/js.js
@@ -52,3 +52,14 @@ function shark(yourDistence=3, sharkDistence=6,
 	return "Dead";		
 }
 }
+document.write("пипипулыка");
+let str = "s1ba1v1";
+function answer(){
+	let odin = 0;
+	for (let i=0;i<=str.length;i++){
+	if(1 ==str[i]){
+		odin+=1
+	}	
+	}
+	document.write(odin);
+}
